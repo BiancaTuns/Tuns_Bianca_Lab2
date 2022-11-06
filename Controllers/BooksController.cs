9f10c6ce25ef1dafa931094743164aa2ee31f591
Bianca Tuns <bia.tuns24@gmail.com>
2022-11-06 21:58:43 +0200

    Laborator3

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -35,8 +35,11 @@ namespace Tuns_Bianca_Lab2.Controllers
             }
 
             var book = await _context.Books
-                .Include(b => b.Author)
+                .Include(s => s.Orders)
+                .ThenInclude(e => e.Customer)
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (book == null)
             {
                 return NotFound();
@@ -64,15 +67,24 @@ namespace Tuns_Bianca_Lab2.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,AuthorID,Price")] Book book)
+        public async Task<IActionResult> Create([Bind("Title,Author,Price")] Book book)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(book);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(book);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+
             }
-            ViewData["AuthorID"] = new SelectList(_context.Set<Authors>(), "Id", "Id", book.AuthorID);
+            catch (DbUpdateException /* ex*/)
+            {
+                ModelState.AddModelError("", "Unable to save changes." +
+                    "Try again, and if the problem persist ");
+            }
+            
             return View(book);
         }
 
@@ -96,37 +108,35 @@ namespace Tuns_Bianca_Lab2.Controllers
         // POST: Books/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,AuthorID,Price")] Book book)
+        public async Task<IActionResult> EditPost(int? id/*, [Bind("Id,Title,AuthorID,Price")] Book book*/)
         {
-            if (id != book.Id)
+            if (id == null)
             {
                 return NotFound();
             }
-
-            if (ModelState.IsValid)
+            var bookToUpdate = await _context.Books.FirstOrDefaultAsync(s => s.Id == id);
+            if (await TryUpdateModelAsync<Book>(
+                bookToUpdate,
+                "",
+                s =>s.Author, s =>s.Title, s =>s.Price)
+                )
             {
                 try
                 {
-                    _context.Update(book);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException /* ex */)
                 {
-                    if (!BookExists(book.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    ModelState.AddModelError("", "Unable to save changes." +
+                        "Try again, abd if the problem persists ");
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AuthorID"] = new SelectList(_context.Set<Authors>(), "Id", "Id", book.AuthorID);
-            return View(book);
+           /* ViewData["AuthorID"] = new SelectList(_context.Set<Authors>(), "Id", "Id", book.AuthorID);*/
+            return View(bookToUpdate);
         }
 
         // GET: Books/Delete/5
@@ -151,20 +161,25 @@ namespace Tuns_Bianca_Lab2.Controllers
         // POST: Books/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
         {
-            if (_context.Books == null)
+            if (id == null)
             {
-                return Problem("Entity set 'LibraryContext.Books'  is null.");
+                return NotFound();
             }
-            var book = await _context.Books.FindAsync(id);
+            var book = await _context.Books
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (book != null)
             {
-                _context.Books.Remove(book);
+               return NotFound();
             }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            if(saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete fain=led. Try again.";
+            }
+            return View(book);
         }
 
         private bool BookExists(int id)
